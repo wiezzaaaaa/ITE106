87893bf45ced9f5a8dcc65c6be2d5fba97fd0f24
Riezza Mae Rogacion <rogacionriezzamae@gmail.com>
2024-10-15 16:44:03 +0800

    Add files via upload

---
+++ StudentGrade.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+	public class StudentGrade {
+		public static void main(String[]args) {
+		
+		System.out.println("Enter the number of students in the class:");ja
+		Scanner scanner = new Scanner(System.in);
+		
+		int numStudents = scanner.nextInt();
+		scanner.nextLine();
+		
+		String[] studentNames = new String[numstudents];
+		double[] studentaverages = new double [numstudents];
+		char [] studentgrades = new char [numstudents];
+		
+		for(int i = 0 ; i< numstudents; i++) {
+			System.out.println("Enter details for students" + (i +1));
+			
+			System.out.print("Name: ");
+		
+				}
+			}
+	}
