96357707629e137ef64629abeca908dcae5db0d6
Riezza Mae Rogacion <rogacionriezzamae@gmail.com>
2024-11-21 16:17:33 +0800

    Create MenuExample.java

---
+++ Act4/MenuExample.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class MenuExample {
+	public static void main (String [] args) {
+	JFrame frame = new JFrame ("My Frame");
+	frame.setDefaultCloseOperation (
+	JFrame.EXIT_ON_CLOSE);
+	JMenu fileMenu = new JMenu ("File");
+	fileMenu.add(new JMenuItem ("New"));
+	fileMenu.add(new JMenuItem ("Open"));
+	fileMenu.add(new JMenuItem ("Close"));
+	
+	item.addActionListener(new MenuActionListener());
+	fileMenu.add(item);
+	
+	JMenu editMenu = new JMenu ("Edit");
+	editMenu.add(new JMenuItem("Undo"));
+	editMenu.add(new JMenuItem("Redo"));
+	editMenu.add(new JMenuItem("Cut"));
+	
+	JMenuBar menubar = new JMenuBar();
+	menubar.add(fileMenu);
+	menubar.add(editMenu);
+	
+	frame.setJMenuBar(menubar);
+	frame.setVisible(true);
+} }
